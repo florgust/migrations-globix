16ddefbaf51019371fe2e2732221562647870b95
florgust <luan.flor05@gmail.com>
2025-03-18 20:20:20 -0300

    SCRUM-60: Criando tabela usuario e viagem

---
+++ migrations/20250318225638-create-table-usuario.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('usuarios', {
+      id: {
+        type: Sequelize.SMALLINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nome: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true
+      },
+      senha: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      tipo: {
+        type: Sequelize.ENUM('participante', 'organizador'),
+        allowNull: false,
+        defaultValue: 'participante'
+      },
+      status: {
+        type: Sequelize.SMALLINT,
+        allowNull: false,
+        defaultValue: 1
+      },
+      dataCriacao: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      dataAtualizada: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('usuarios');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_usuarios_tipo";'); // Remove o tipo ENUM
+  }
+};
---
+++ migrations/20250318231238-create-table-viagem.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('viagens', {
+      id_viagem: {
+        type: Sequelize.SMALLINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nome: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      descricao: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      data_inicio: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      data_fim: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      criador_id: {
+        type: Sequelize.SMALLINT,
+        allowNull: false,
+        references: {
+          model: 'usuarios', // Nome da tabela referenciada
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      codigo_convite: {
+        type: Sequelize.SMALLINT,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.SMALLINT,
+        allowNull: false,
+        defaultValue: 1
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('viagens');
+  }
+};
