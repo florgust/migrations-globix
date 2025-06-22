b356630af16c71c84eea9e8adf3681e2492d0ae6
Nicolas Evangelista <nicolasleocadio2010@gmail.com>
2025-06-22 02:13:14 -0300

    SCRUM-158: ajuste config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
-        use_env_variable: 'DATABASE_URL',
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+      	require: false,
+	rejectUnauthorized: false
+      }
     }
+  }
 };
