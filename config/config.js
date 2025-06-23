895ae8fcd036d386b12649b83dc4f66f34c1616c
florgust <luan.flor05@gmail.com>
2025-06-23 09:36:10 -0300

    SCRUM-158: ajuste no config.js - necessario alterar .env

--- config/config.js
+++ config/config.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-      	require: false,
-	rejectUnauthorized: false
-      }
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: false
+        }
     }
-  }
 };
+
